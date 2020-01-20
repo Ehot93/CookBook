41f07226eb3efda6bd1ee7f12306da4bb99b3eda
Vlad-home <vlad3x@mail.ru>
2020-01-20 22:44:52 +0700

    'init'

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+{id: 11, name: 'Mr.Sonic'},
+{id: 12, name: 'Narco'},
+{id: 13, name: 'superBob'},
+{id: 14, name: 'megaCris'},
+{id: 15, name: 'Dr Dev'},
+{ id: 16, name: 'RubberMan' },
+{ id: 17, name: 'Dynama' },
+{ id: 18, name: 'Dr IQ' },
+{ id: 19, name: 'Magma' },
+{ id: 20, name: 'Tornado' }
+];
